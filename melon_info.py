3a6aab0eb42296692006b8d5b1228197227d6388
Crystal Lopez <crystallopez289@gmail.com>
2020-05-18 22:37:23 +0000

    fixed typos in melons.py, updated melon_info.py to print all melon info

--- melon_info.py
+++ melon_info.py
@@ -4,15 +4,21 @@
 from melons import melons
 
 
-def print_melon(name, seedless, price):
-    """Print each melon with corresponding attribute information."""
+# def print_melon(name, seedless, price):
+#     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+#     have_or_have_not = 'have'
+#     if seedless:
+#         have_or_have_not = 'do not have'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+#     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+
+def print_melon(melons):
+    for melon in melons:
+        print(melon)
+        for attribute in melons[melon]:
+            print(f"{attribute}:{melons[melon][attribute]}")
